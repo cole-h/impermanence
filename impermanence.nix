c9f6cadbc75017c021fff3f3b65537a07f19e8fd
Bernardo Meurer <meurerbernardo@gmail.com>
2020-06-06 17:36:48 -0700

    impermanence: correctly replicate permissions of directory structures

--- impermanence.nix
+++ impermanence.nix
@@ -52,22 +52,63 @@ in
       let
         # Function to create a directory in both the place where we want
         # to bind mount it as well as making sure it exists in the location
-        # where persistence is located.
+        # where persistence is located. This also enforces the correct ownership
+        # of the directory structure.
         mkDirCreationSnippet = persistentStoragePath: dir:
           ''
-            mkdir -p "${dir}" "${persistentStoragePath}${dir}"
+            # capture the nix vars into bash to avoid escape hell
+            sourceBase="${persistentStoragePath}"
+            target="${dir}"
+            # trim trailing slashes the root of all evil
+            sourceBase="''${sourceBase%/}"
+            target="''${target%/}"
+            # iterate over each part of the target path
+            previousPath="/"
+            for pathPart in $(echo "$target" | tr "/" " "); do
+              # construct the incremental path, e.g. /var, /var/lib, /var/lib/iwd
+              currentTargetPath="$previousPath$pathPart/"
+              # construct the source path, e.g. /state/var, /state/var/lib
+              currentSourcePath="$sourceBase$currentTargetPath"
+              if [ ! -e "$currentSourcePath" ]; then
+                printf "Bind source '%s' does not exist, creating it\n" "$currentSourcePath"
+                mkdir "$currentSourcePath"
+              fi
+              if [ ! -e "$currentTargetPath" ]; then
+                mkdir "$currentTargetPath"
+              fi
+              # synchronize perms between the two, should be a noop if they were
+              # both just created.
+              chown --reference="$currentSourcePath" "$currentTargetPath"
+              # lastly we update the previousPath to continue down the tree
+              previousPath="$currentTargetPath"
+              unset currentSourcePath
+              unset currentTargetPath
+            done
+            unset previousPath
+            unset sourceBase
+            unset target
           '';
 
         # Function to create a file in both the place where we want to bind
         # mount it as well as making sure it exists in the location where
-        # persistence is located.
+        # persistence is located. This is simply mkDirCreationSnippet on the
+        # dirname with a touch and chown.
         mkFileCreationSnippet = persistentStoragePath: file:
-          let
-            targetFile = "${persistentStoragePath}${file}";
-          in
           ''
-            mkdir -p $(dirname "${file}") $(dirname "${targetFile}") &&
-              touch "${file}" "${targetFile}"
+            # replicate the directory structure of ${file}
+            ${mkDirCreationSnippet persistentStoragePath (dirOf file)}
+            # now create the source and target, if they don't exist
+            sourcePath="${persistentStoragePath}${file}"
+            targetPath="${file}"
+            if [ ! -e "$sourcePath" ]; then
+              touch "$sourcePath"
+            fi
+            if [ ! -e "$targetPath" ]; then
+              touch "$targetPath"
+            fi
+            # lastly we correctly chown them, if both were just created this
+            # should be a noop.
+            chown --reference="$sourcePath" "$targetPath"
           '';
 
         # Function to build the activation script string for creating files
